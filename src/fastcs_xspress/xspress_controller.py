e511f535c7c514a787b82ce8836c4cf1f86d5e89
Gary Yendell <gdy@live.co.uk>
2026-02-03 16:53:28 +0000

    Update to fastcs 0.12 (#4)
    * Update to fastcs 0.12
    
    * Enable TRACE logging
    
    * add _create_commands and fix missing API prefix
    
    ---------
    
    Co-authored-by: Luis Segalla <Luis.Segalla@diamond.ac.uk>

--- src/fastcs_xspress/xspress_controller.py
+++ src/fastcs_xspress/xspress_controller.py
@@ -1,46 +1,27 @@
-from fastcs.connections.ip_connection import IPConnectionSettings
-from fastcs.controller import Controller
-from fastcs.datatypes import Bool, Float, Int, String
+from fastcs.controllers import BaseController
+from fastcs_odin.controllers import OdinController
 from fastcs_odin.http_connection import HTTPConnection
-from fastcs_odin.util import create_odin_parameters
+from fastcs_odin.util import OdinParameter
 
 from fastcs_xspress.xspress_adapter_controller import XspressAdapterController
 
-types = {"float": Float(), "int": Int(), "bool": Bool(), "str": String()}
 
-REQUEST_METADATA_HEADER = {"Accept": "application/json;metadata=true"}
-
-
-class AdapterResponseError(Exception): ...
-
-
-class XspressController(Controller):
+class XspressController(OdinController):
     """A root ``Controller`` for an xspress control server."""
 
-    API_PREFIX = "api/0.1"
+    def _create_adapter_controller(
+        self,
+        connection: HTTPConnection,
+        parameters: list[OdinParameter],
+        adapter: str,
+        module: str,
+    ) -> BaseController:
+        match module:
+            case "XspressAdapter":
+                return XspressAdapterController(
+                    connection, parameters, f"{self.API_PREFIX}/{adapter}", self._ios
+                )
 
-    def __init__(self, settings: IPConnectionSettings) -> None:
-        super().__init__()
-
-        self.connection = HTTPConnection(settings.ip, settings.port)
-
-    async def connect(self) -> None:
-        self.connection.open()
-
-    async def initialise(self) -> None:
-        await self.connect()
-
-        response = await self.connection.get(
-            f"{self.API_PREFIX}/xspress", headers=REQUEST_METADATA_HEADER
+        return super()._create_adapter_controller(
+            connection, parameters, adapter, module
         )
-
-        adapter_controller = XspressAdapterController(
-            self.connection,
-            create_odin_parameters(response),
-            f"{self.API_PREFIX}/xspress",
-        )
-
-        self.register_sub_controller("XSPRESS", adapter_controller)
-        await adapter_controller.initialise()
-
-        await self.connection.close()
