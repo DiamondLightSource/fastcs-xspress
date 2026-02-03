bdfcdac6460c5097a66fc2ea3fdb3dd2b7d7bf74
Luis Segalla <Luis.Segalla@diamond.ac.uk>
2026-02-03 11:54:49 +0000

    WIP: Split Status and Config parameters into a subcontroller and cleaned the screens a bit

--- src/fastcs_xspress/xspress_adapter_controller.py
+++ src/fastcs_xspress/xspress_adapter_controller.py
@@ -1,5 +1,6 @@
 from fastcs_odin.controllers.odin_adapter_controller import OdinAdapterController
-from fastcs_odin.util import unpack_status_arrays
+from fastcs_odin.controllers.odin_subcontroller import OdinSubController
+from fastcs_odin.util import partition, unpack_status_arrays
 
 uri_list = [
     ["status", "scalar_0"],
@@ -19,10 +20,44 @@ uri_list = [
 class XspressAdapterController(OdinAdapterController):
     """SubController for an Xspress adapter in an odin control server."""
 
+    _subcontroller_cls: type[OdinSubController] = OdinSubController
+
     async def initialise(self):
+        # Unpack all the status parameters
         self.parameters = unpack_status_arrays(
             parameters=self.parameters, uris=uri_list
         )
 
+        # Change path for all status and config parameters
+        for parameter in self.parameters:
+            # Remove 0 index and status/config
+            match parameter.uri:
+                case ["status" | "config", *_]:
+                    parameter.set_path(parameter.path[1:])
+
+        status_parameters, self.parameters = partition(
+            self.parameters, lambda p: "status" in p.uri
+        )
+
+        config_parameters, self.parameters = partition(
+            self.parameters, lambda p: "config" in p.uri
+        )
+
+        self.status_controller = self._subcontroller_cls(
+            self.connection,
+            status_parameters,
+            f"{self._api_prefix}",
+            self._ios,
+        )
+
+        self.config_controller = self._subcontroller_cls(
+            self.connection,
+            config_parameters,
+            f"{self._api_prefix}",
+            self._ios,
+        )
+
+        await self.status_controller.initialise()
+        await self.config_controller.initialise()
         await self._create_attributes()
         await self._create_commands()
