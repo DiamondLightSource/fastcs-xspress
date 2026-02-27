96a2c6a7c58fae6caae91b59c359cb7543b3b650
Luis Segalla <Luis.Segalla@diamond.ac.uk>
2026-02-27 09:42:36 +0000

    Add on_update_callback for chunks and logic to cycle through all MCAs

--- src/fastcs_xspress/xspress_fp_adapter_controller.py
+++ src/fastcs_xspress/xspress_fp_adapter_controller.py
@@ -1,38 +1,41 @@
 import logging
 
-from fastcs.attributes import AttrR, AttrRW
-from fastcs.datatypes import Int
+from fastcs.attributes import AttrRW
 from fastcs_odin.controllers import (
     FrameProcessorAdapterController,
 )
 from fastcs_odin.controllers.odin_subcontroller import OdinSubController
-from fastcs_odin.io import ConfigFanAttributeIORef
 from fastcs_odin.util import get_all_sub_controllers
 
 
 class XspressFPAdapterController(FrameProcessorAdapterController):
-    state = AttrR[str]
-    chunk_total = AttrRW[int]
+    chunks: AttrRW[int]
 
     async def initialise(self):
         await super().initialise()
+        # self.chunks.enable_tracing()
 
-        attr_list = []
-
-        subcontrollers = get_all_sub_controllers(self)
-
-        for sub_controller in subcontrollers:
+        # Construct a list with all the MCA dataset chunks
+        mca_list = []
+        for sub_controller in get_all_sub_controllers(self):
             match sub_controller:
                 case OdinSubController():
                     for parameter in sub_controller.parameters:
-                        if ("chunks" in parameter.uri and "0" in parameter.uri) or (
-                            "xspress" in parameter.uri and "chunks" in parameter.uri
-                        ):
-                            attr_list.append(sub_controller.attributes[parameter.name])
+                        if "chunks" in parameter.uri and "0" in parameter.uri:
+                            mca_list.append(sub_controller.attributes[parameter.name])
 
                 case _:
                     logging.warning(
                         f"Subcontroller {sub_controller} not an OdinAdapterController"
                     )
-        chunk_total = AttrRW(Int(), ConfigFanAttributeIORef(attr_list))
-        self.add_attribute("TotalChunk", chunk_total)
+
+        async def set_chunk_fp(value: int):
+            for mca in mca_list:
+                fp_num = mca.full_name.split(".")[1]
+                num_num = mca.full_name.split(".")[-1].split("_")[1]
+                await self.connection.put(
+                    f"api/0.1/fp/{fp_num}/config/hdf/dataset/mca_{num_num}/chunks",
+                    [value, 1, 4096],
+                )
+
+        self.chunks.add_on_update_callback(callback=set_chunk_fp)
