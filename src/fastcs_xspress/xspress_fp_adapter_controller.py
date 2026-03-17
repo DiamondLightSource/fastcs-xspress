c4934cbaddae418720f49433f02d5bc460dfed9a
Luis Segalla <Luis.Segalla@diamond.ac.uk>
2026-03-17 10:36:41 +0000

    Updated logic for mca_list

--- src/fastcs_xspress/xspress_fp_adapter_controller.py
+++ src/fastcs_xspress/xspress_fp_adapter_controller.py
@@ -14,7 +14,6 @@ class XspressFPAdapterController(FrameProcessorAdapterController):
 
     async def initialise(self):
         await super().initialise()
-        # self.chunks.enable_tracing()
 
         # Construct a list with all the MCA dataset chunks
         mca_list = []
@@ -23,19 +22,19 @@ class XspressFPAdapterController(FrameProcessorAdapterController):
                 case OdinSubController():
                     for parameter in sub_controller.parameters:
                         if "chunks" in parameter.uri and "0" in parameter.uri:
-                            mca_list.append(sub_controller.attributes[parameter.name])
-
+                            # mca_list.append(sub_controller.attributes[parameter.name])
+                            mca_list.append(
+                                (sub_controller.path[1], parameter.name.split("_")[1])
+                            )
                 case _:
                     logging.warning(
                         f"Subcontroller {sub_controller} not an OdinAdapterController"
                     )
 
         async def set_chunk_fp(value: int):
-            for mca in mca_list:
-                fp_num = mca.full_name.split(".")[1]
-                num_num = mca.full_name.split(".")[-1].split("_")[1]
+            for sub_controller, mca_num in mca_list:
                 await self.connection.put(
-                    f"api/0.1/fp/{fp_num}/config/hdf/dataset/mca_{num_num}/chunks",
+                    f"api/0.1/fp/{sub_controller}/config/hdf/dataset/mca_{mca_num}/chunks",
                     [value, 1, 4096],  # pyright: ignore[reportArgumentType]
                 )
 
